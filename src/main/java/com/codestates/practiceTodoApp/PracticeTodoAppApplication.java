5ce24b5dc67f9bc6b69e0079105aac1587a08317
LegendPaino <87295495+LegendPaino@users.noreply.github.com>
2023-02-14 16:51:17 +0900

    Spring initializr

---
+++ src/main/java/com/codestates/practiceTodoApp/PracticeTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practiceTodoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeTodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/practiceTodoApp/PracticeTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.practiceTodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
